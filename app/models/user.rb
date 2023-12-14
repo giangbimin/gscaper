26a9a1b9474e706fd2720f41223499bfe92b5c23
giangbimin <gianghoang.dev@gmail.com>
2023-12-14 16:24:27 +0700

    [feature_authentication] update devise view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  #  :recoverable, :rememberable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
+  #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable, :recoverable, :rememberable
 end
