51533eee39997bd07e1c579201582aeec4e6b832
giangbimin <gianghoang.dev@gmail.com>
2023-12-18 13:19:45 +0700

    [feature][user_keywords] add relationship

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,6 @@
 class Keyword < ApplicationRecord
   validates :content, presence: true
   enum status: { pending: 0, processed: 1, failed: 2 }
+  has_many :user_keywords, dependent: :destroy
+  has_many :keywords, through: :user_keywords
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable, :recoverable, :rememberable
+  has_many :user_keywords, dependent: :destroy
 end
